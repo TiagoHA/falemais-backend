8ec8f0665dc50de1ad67cdcd51b8f77112cbc4b8
Tiago Almeida <Tiagu.Almeida@gmail.com>
2020-12-11 10:12:46 +0000

    ✨ adding main and typeorm configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PhonePlansModule } from './phone-plans/phone-plans.module';
-import { RateModule } from './rate/rate.module';
-import { PriceReportModule } from './price-report/price-report.module';
+import { typeOrmConfig } from './configs/typeorm.configs';
 
 @Module({
-  imports: [PhonePlansModule, RateModule, PriceReportModule],
+  imports: [TypeOrmModule.forRoot(typeOrmConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+bootstrap();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
+
+  await app.listen(3000, () => console.log('listening on port: 3000'));
 }
-bootstrap();
